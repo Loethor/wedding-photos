30fa4276d1e2d349aa21941a99e21ae1bc88f5a1
loethor <lrb2392@gmail.com>
2026-07-16 19:27:18 +0200

    updated config

--- app/config.py
+++ app/config.py
@@ -1,4 +1,8 @@
+import os
 from pathlib import Path
 
-# PHOTO_STORAGE = Path("/srv/photo-data")
-PHOTO_STORAGE = Path("./storage")
+PHOTO_STORAGE = Path("/srv/photo-data")
+# PHOTO_STORAGE = Path("./storage")
+
+WEDDING_PASSWORD = os.environ["WEDDING_PASSWORD"]
+SECRET_KEY = os.environ["SECRET_KEY"]
